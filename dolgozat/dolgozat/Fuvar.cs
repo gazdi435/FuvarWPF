1e49e9d857e1479aaf7d9f0a4684716bd6bc390f
Zsolesz <72067999+gazdi435@users.noreply.github.com>
2023-09-20 10:08:27 +0200

    Add files via upload

---
+++ dolgozat/dolgozat/Fuvar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolgozat
+{
+    internal class Fuvar
+    {
+        int taxiAzonosito;
+        string indulasIdopont;
+        int idotartam;
+        double megtettTav;
+        double vitelDij;
+        double borravalo;
+        string fizetesiMod;
+
+        public Fuvar(string sor)
+        {
+            this.taxiAzonosito = Convert.ToInt32(sor.Split(';')[0]);
+            this.indulasIdopont = sor.Split(';')[1];
+            this.idotartam = Convert.ToInt32(sor.Split(';')[2]);
+            this.megtettTav = Convert.ToDouble(sor.Split(';')[3]);
+            this.vitelDij = Convert.ToDouble(sor.Split(';')[4]);
+            this.borravalo = Convert.ToDouble(sor.Split(';')[5]);
+            this.fizetesiMod = sor.Split(';')[6];
+        }
+
+        public int TaxiAzonosito { get => taxiAzonosito; }
+        public string IndulasIdopont { get => indulasIdopont; }
+        public int Idotartam { get => idotartam; }
+        public double MegtettTav { get => megtettTav; }
+        public double VitelDij { get => vitelDij; }
+        public double Borravalo { get => borravalo; }
+        public string FizetesiMod { get => fizetesiMod; }
+
+        
+    }
+}
---
+++ dolgozat/dolgozat/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace dolgozat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// balra:Sinka József
+    /// jobbra: Beke Tamás
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+            List<Fuvar> ListaFuvarok = File.ReadAllLines("fuvar.csv").Skip(1).Select(x => new Fuvar(x)).ToList();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListaFuvarok.DistinctBy(x=>x.TaxiAzonosito).ToList().ForEach(x=>CBAzonositok.Items.Add(x.TaxiAzonosito));
+
+        }
+
+        private void utazasokSzama(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("3. feladat: " + ListaFuvarok.Count() + " fuvar");
+        }
+
+        private void megtettUt(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("6. feladat: " + ListaFuvarok.Sum(x => x.MegtettTav) * 1.6 + " km");
+        }
+
+        private void LeghosszabbFuvar(object sender, RoutedEventArgs e)
+        {
+            LBhetes.Items.Add($"Fuvar hossza: {ListaFuvarok.Max(x => x.Idotartam)} másodperc");
+            LBhetes.Items.Add($"Taxi azonosító: { ListaFuvarok.Find(x => x.Idotartam == ListaFuvarok.Max(x => x.Idotartam)).TaxiAzonosito}");
+            LBhetes.Items.Add($"Megtett távolság: {ListaFuvarok.Find(x => x.Idotartam == ListaFuvarok.Max(x => x.Idotartam)).MegtettTav * 1.6} km");
+            LBhetes.Items.Add($" Viteldíj: {ListaFuvarok.Find(x => x.Idotartam == ListaFuvarok.Max(x => x.Idotartam)).VitelDij}");
+        }
+
+        private void Hibak(object sender, RoutedEventArgs e)
+        {
+            ListaFuvarok.Where(x => x.Idotartam > 0 && x.VitelDij > 0 && x.MegtettTav==0).ToList().ForEach(y => File.AppendAllText("Hibak.txt", $"{y.TaxiAzonosito};{y.IndulasIdopont};{y.Idotartam};{y.MegtettTav};{y.VitelDij};{y.Borravalo};{y.FizetesiMod}"));
+
+        }
+
+        private void FizetesiModok(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bevetel(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show($"ListaFuvarok.Count(x=>x.TaxiAzonosito==Convert.ToInt32(CBAzonositok.SelectedIndex))" fuvar alatt: ListaFuvarok.Where(x=>x.TaxiAzonosito==Convert.ToInt32(CBAzonositok.SelectedIndex)).Sum(x=> x.viteldij));
+
+        }
+    }
+}
